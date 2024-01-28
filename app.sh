0bff61d922bf6eca08400cb90d44371d35ef2d73
Derek <72669839+realderekstevens@users.noreply.github.com>
2024-01-28 00:16:34 -0700

    Update app.sh

--- app.sh
+++ app.sh
@@ -11,26 +11,32 @@ MAIN_MENU() {
 	fi
 
 	echo "What would you like to do?"
-	echo -e "\n1. New Hand\n2. Shuffle Deck\n3. Display Card Count\n4. Exit"
+	echo -e "\n1. New Hand\n2. Discard All Cards\n3. Add New Deck\n4. Display Card Count\n5. Exit"
 	read MAIN_MENU_SELECTION
 
 	case $MAIN_MENU_SELECTION in
 		1) NEW_HAND ;;
-		2) SHUFFLE ;;
-		3) DISPLAY_CARD_COUNT ;;
-		4) EXIT ;;
+		2) DISCARD_ALL_CARDS ;;
+		3) ADD_NEW_DECK ;;
+		4) DISPLAY_CARD_COUNT ;;
+		5) EXIT ;;
 		*) MAIN_MENU "Please enter a valid option." ;;
 	esac
 }
 
-
-
-NEW_HAND_MENU(){
+NEW_HAND(){
 echo -e "\nThank you for stopping in.\n"
 }
 
-SHUFFLE(){
-echo -e "\nThank you for stopping in.\n"
+DISCARD_ALL_CARDS(){
+echo -e "\nDiscarding all cards.\n"
+$($PSQL "DELETE FROM CARDS *;") 
+}
+
+
+ADD_NEW_DECK(){
+echo -e "\nAdding new deck.\n"
+$($PSQL "INSERT INTO cards(suit, face, value) VALUES('hearts', 'Ace', 1)") 
 }
 
 DISPLAY_CARD_COUNT(){
